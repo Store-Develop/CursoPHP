826f01bb7cc56279098f06982d1fb339c51b8fab
Juan Perez <AnalistaBI@pulpafruit.com>
2022-08-05 11:37:31 -0500

    Funciones en php

--- prueba.php
+++ prueba.php
@@ -1,8 +1,15 @@
 <?php
-//Ciclo foreach en php foreach(nombreArreglo as nombreClave => nombreValor).
-$futbol = array('barcelona' => 'messi', 'juventus' => 'cuadrado', 'realMadrid' => 'elBicho', 'tottenham' => 'kane');
-
-foreach ($futbol as $dato => $valor) {
-    echo $dato.': '.$valor.'; <br>';
+//Funciones en php.
+function estudiante($nombre, $apellido, $edad, $escuela){
+    echo "nuevo estudiante: $nombre $apellido Edad: $edad Escuela: $escuela";
+    
 }
+estudiante('Juan', 'perez', 24, 'Politecnico Internacional');
+
+function suma($operandoA, $operandoB){
+    return $operandoA + $operandoB;
+}
+
+$resultado = suma(45, 50);
+echo "El resultado de su suma es: $resultado.";
 ?> 
