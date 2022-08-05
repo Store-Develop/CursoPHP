7712be5e79b9f07f007322ce35c9d7e8d87c5150
Juan Perez <AnalistaBI@pulpafruit.com>
2022-08-05 09:51:20 -0500

    Funciones con Arrays en php.

--- prueba.php
+++ prueba.php
@@ -1,106 +1,45 @@
 <?php
+// Ejercicios con array.
 
-//Varibles tipo string, (cadena de texto)
-$nombre = 'Juan';
-$apellido = 'perez';
+$cursos = array('python<br>', 'java<br>', 'php<br>', 'html<br>', 'css<br>' );
+echo $cursos[3];
+// eliminar o reemplazar valores en un arreglo por medio del indice o posición.
+$cursos[3] = 'javascript<br>';
+//Añadir valor a un arreglo por posición.
+$cursos[5] = 'php<br>';
+//Imprimir los valores de un arreglo.
+print_r($cursos);
+echo count($cursos);
 
-//Variables tipo number
-$numero = 3;
-$numero2 = 3.2;
-
-//impresión de en pantalla se puede utilizar *echo* y se puede utilizar *print*
-//impresion variable a variable
-print $nombre;
-print $numero;
-
-//Impresión de variables individul o anidadas en php
-// con las comillas dobles se puede ingresar las variables junto al texto.
-echo "Nombre completo: $nombre, $apellido";
-
-//Con comillas sencillas toca concatenar las variables
-echo 'Nombre: '.$nombre. ' '.$apellido;
-
-//Constantes en PHP
-define('curso', 'PHP');
-echo curso;
-
-/*
-Esto nos marca un error debido ya es una constante y su valor no puede ser modificado.
-define('curso', 'Python');
-echo curso;
-*/
-
-
-// Condicionales php
-$numero_uno = 5;
-$numero_dos = 10;
-
-if ($numero_uno < $numero_dos) {
-    echo 'Curso de php bb';
+//Otra forma de general un arreglo.
+$meses[0] = 'Enero';
+$meses[1] = 'Febrero';
+$meses[2] = 'Marzo';
+$meses[3] = 'Abril';
+$meses[4] = 'Mayo';
+$meses[5] = 'Junio';
+$meses[6] = 'Julio';
+$meses[7] = 'Agosto';
+$meses[8] = 'Septiembre';
+$meses[9] = 'Octubre';
+$meses[10] = 'Noviembre';
+$meses[11] = 'Diciembre';
+echo '<br>Cantidad de elementos en el Array: ' .count($meses);
+for ($i=0; $i <count($meses); $i++) { 
+    //echo $meses[$i];
+    if ($meses[$i] == 'Agosto') {
+        echo '<br>Mes de victoria Trabajo como Desarrollador Web.';
+    }
 }
-
-elseif($numero_uno == $numero_dos){
-    echo "El numero $numero_uno es igual al $numero_dos";
-}else{
-    echo 'Curso de python';
-}
-
-//switch
-$cursoS = 'java';
-switch ($cursoS) {
-    case 'php':
-        echo 'Lunes y Martes';
-        break;
-    case 'java':
-            echo 'Miercoles';
-        break;
-    case 'python':
-        echo 'jueves';
-        break;
-    case 'javascript':
-        echo 'viernes';
-        break;
-    default:
-        echo 'Lenguaje no matriculado este semestre.';
-        break;
-}
-
-//Ciclos en php
-
-// while
-
-$numeroE = 1;
-while ($numeroE <= 10) {
-    echo $numeroE;
-
-    $numeroE += 1;
-}
-
-// do while
-do {
-    echo $numeroE;
-    $numeroE += 2;
-} while ($numeroE <= 30);
-
-// Ciclo for
-
-for ($numeroE=0; $numeroE <= 50; $numeroE += 5) { 
-    echo $numeroE;
-}
-
-
-//Arrays, Vectores, Arreglos en php
-//Array normal
-$dias = array('Lunes <br>','Martes <br>', 'Miercoles <br>', 'Jueves <br>', 'viernes <br>', 'Sabado <br>', 'Domingo <br>');
-//Formas de imprimir un array 1. var_dump
-var_dump($dias);
-// 2 print_r
-print_r($dias);
-
-
-//array asociativo
-$estudiante = array('nombre' => 'Juan<br>', 'apellido' => 'Perez<br>', 'Edad' => 24 );
-print_r($estudiante);
-echo $estudiante[3];
+ //Recorriendo arreglo e imprimiendo el ante penultimo valor.
+ $panas = array('andres','jesus','julian','johan','bryam','pinena','felipe','jose','yirmer' );
+ $panas[9] = 'lorena';
+ $posicion = count($panas) -2;
+ //echo $amigos[$posicion];
+ //rsort(nombre del arreglo); Invierte una lista y muestra el ultimo valor de primera y el prilero de ultimo.
+ // arsort(nombre del arreglo) mantiene el indice que le corresponde por defect a cada valor desde el inicio.
+ //sin importar a donde se mueva el valor dentro del mismo Array.
+ asort($panas);
+ print_r($panas);
 
 ?> 
